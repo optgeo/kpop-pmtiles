c63668eb969b7f6399fb2d6f15359874303ef399
Hidenori <hfu@users.noreply.github.com>
2024-08-19 15:41:16 +0900

    attempted to improve efficiency

--- create_db.rb
+++ create_db.rb
@@ -2,19 +2,24 @@ require 'h3'
 require 'json'
 require 'lmdb'
 
-$db = LMDB.new('db', :mapsize => 1000 * 1024 * 1024 * 1024).database
+env = LMDB.new('db', :mapsize => 1000 * 1024 * 1024 * 1024)
+$db = []
+0.upto(8) {|resolution|
+  $db.push(env.database(resolution.to_s))
+}
 
+start_time = Time.now
 count = 0
 while gets
   f = JSON.parse($_)
   h3 = H3.from_string(f['properties']['h3'])
   pop = f['properties']['population'].to_i
   H3.resolution(h3).downto(0) {|resolution|
-    h3h = H3.to_string(H3.parent(h3, resolution))
-    poph = $db[h3h].to_i + pop
-    $db[h3h] = poph.to_s
+    h3h = [H3.parent(h3, resolution)].pack('Q>')
+    poph = $db[resolution][h3h]
+    $db[resolution][h3h] = [poph.nil? ? pop : poph.unpack('Q>').first + pop].pack('Q>')
   }
   count += 1
-  print "#{count}\r" if count % 1000 == 0
+  print "#{count} #{(count / (Time.now - start_time)).round}r/s\r" if count % 1000 == 0
 end
 
--- dump_db.rb
+++ dump_db.rb
@@ -6,22 +6,25 @@ LAYER = 'kpop'
 MINZOOMS = [0, 2, 3, 4, 5, 6, 7, 8, 9]
 MAXZOOMS = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-$db = LMDB.new('db', :mapsize => 1000 * 1024 * 1024 * 1024).database
+env = LMDB.new('db', :mapsize => 1000 * 1024 * 1024 * 1024)
 
-$db.each {|h3s, pops|
-  h3 = H3.from_string(h3s)
-  pop = pops.to_i
-  resolution = H3.resolution(h3)
-  f = {
-    :type => 'Feature',
-    :geometry => JSON.parse(H3.coordinates_to_geo_json([H3.to_boundary(h3)])),
-    :properties => { :pop => pop, :h3 => h3s },
-    :tippecanoe => {
-      :layer => LAYER,
-      :minzoom => MINZOOMS[resolution],
-      :maxzoom => MAXZOOMS[resolution]
+0.upto(8) {|resolution|
+  db = env.database(resolution.to_s)
+  db.each {|h3p, popp|
+    h3 = h3p.unpack('Q>').first
+    pop = popp.unpack('Q>').first
+    resolution = H3.resolution(h3)
+    f = {
+      :type => 'Feature',
+      :geometry => JSON.parse(H3.coordinates_to_geo_json([H3.to_boundary(h3)])),
+      :properties => { :pop => pop },
+      :tippecanoe => {
+        :layer => LAYER,
+        :minzoom => MINZOOMS[resolution],
+        :maxzoom => MAXZOOMS[resolution]
+      }
     }
+    print "#{JSON.dump(f)}\n"
   }
-  print "#{JSON.dump(f)}\n"
 }
 
